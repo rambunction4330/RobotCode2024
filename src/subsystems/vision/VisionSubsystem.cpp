ef035155168f3d154ee5567613140b044a232d57
notSam25 <notSam25@users.noreply.github.com>
2024-02-05 19:19:51 +0000

    BOT: Apply Formatting

--- src/subsystems/vision/VisionSubsystem.cpp
+++ src/subsystems/vision/VisionSubsystem.cpp
@@ -4,50 +4,42 @@
 
 #include "subsystems/vision/VisionSubsystem.h"
 
-VisionSubsystem::VisionSubsystem()
-{
+VisionSubsystem::VisionSubsystem() {
 
   // TODO: Apply camera transormations relative to robot centre.
-  frc::Transform3d robotToCam =
-      frc::Transform3d(frc::Translation3d(0_m, 0_m, 0_m),
-                       frc::Rotation3d(0_rad, 0_rad, 0_rad));
+  frc::Transform3d robotToCam = frc::Transform3d(
+      frc::Translation3d(0_m, 0_m, 0_m), frc::Rotation3d(0_rad, 0_rad, 0_rad));
 
-  frc::AprilTagFieldLayout aprilTags = frc::LoadAprilTagLayoutField(frc::AprilTagField::k2024Crescendo);
-  this->m_PoseEstimator = new photon::PhotonPoseEstimator(aprilTags,
-                                                          photon::MULTI_TAG_PNP_ON_COPROCESSOR,
-                                                          photon::PhotonCamera("TESTCAMERA"),
-                                                          robotToCam);
-
+  frc::AprilTagFieldLayout aprilTags =
+      frc::LoadAprilTagLayoutField(frc::AprilTagField::k2024Crescendo);
+  this->m_PoseEstimator = new photon::PhotonPoseEstimator(
+      aprilTags, photon::MULTI_TAG_PNP_ON_COPROCESSOR,
+      photon::PhotonCamera("TESTCAMERA"), robotToCam);
 }
 
-VisionSubsystem::~VisionSubsystem() {
-  delete this->m_PoseEstimator;
-}
+VisionSubsystem::~VisionSubsystem() { delete this->m_PoseEstimator; }
 
-std::pair<frc::Pose3d, units::millisecond_t> VisionSubsystem::getEstimatedGlobalPose(
-    frc::Pose3d prevEstimatedRobotPose)
-{
+std::pair<frc::Pose3d, units::millisecond_t>
+VisionSubsystem::getEstimatedGlobalPose(frc::Pose3d prevEstimatedRobotPose) {
   this->m_PoseEstimator->SetReferencePose(prevEstimatedRobotPose);
   units::millisecond_t currentTime = frc::Timer::GetFPGATimestamp();
-  std::optional<photon::EstimatedRobotPose> result = this->m_PoseEstimator->Update();
-  if (result.has_value())
-  {
+  std::optional<photon::EstimatedRobotPose> result =
+      this->m_PoseEstimator->Update();
+  if (result.has_value()) {
     return std::make_pair<>(result->estimatedPose,
                             currentTime - result->timestamp);
-  }
-  else
-  {
+  } else {
     return std::make_pair(frc::Pose3d(), 0_ms);
   }
 }
 
-void VisionSubsystem::Periodic()
-{
+void VisionSubsystem::Periodic() {
   // I know these are errors, they are placeholders.
-  std::pair<frc::Pose3d, units::millisecond_t> result = getEstimatedGlobalPose(drivesubsystem->getestimatedglobalpose);
-  if (result.second != 0_ms)
-  {
-    printf("%.3f, %.3f, %.3f", result.first.X().value(), result.first.Y().value(), result.first.Z().value());
+  std::pair<frc::Pose3d, units::millisecond_t> result =
+      getEstimatedGlobalPose(drivesubsystem->getestimatedglobalpose);
+  if (result.second != 0_ms) {
+    printf("%.3f, %.3f, %.3f", result.first.X().value(),
+           result.first.Y().value(), result.first.Z().value());
     drivesubsystem->appendglobalpose(result.first)
   }
 }
